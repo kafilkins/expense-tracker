9d0ca45a946815a019ae2624b4e4638a6b639c93
Keith Filkins <kafilkins@me.com>
2021-08-23 17:11:53 -0400

    Connected everything and enabled clearing input fields after submit.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
+import { GlobalProvider } from './context/GlobalState';
+
 function App() {
   return (
-    <div className="App">
+    <GlobalProvider>
       <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
-    </div>
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
   );
 }
 
